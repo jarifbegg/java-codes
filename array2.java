add25dc5869811d832e3f08e362013eed7870622
jarifbegg <67698116+jarifbegg@users.noreply.github.com>
2023-09-01 10:02:32 +0530

    Add files via upload

---
+++ array2.java
@@ -0,0 +1,23 @@
+package array2;
+
+public class array2 {
+	    public static void main(String[] args) {
+
+	        int[][] twoDArray = {
+	                { 30 },
+	                { 300, 500 },
+	                { 3000, 5000, 5000 }
+	        };
+
+	        System.out.println("2D Array:");
+	        for (int i = 0; i < twoDArray.length; i++) {
+	            for (int j = 0; j < twoDArray[i].length; j++) {
+	                System.out.print(twoDArray[i][j] + " ");
+	            }
+	            System.out.println();
+	        }
+	    }
+	
+
+
+}
